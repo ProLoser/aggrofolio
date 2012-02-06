3d8b91c3b15f280706ee38c2ce65c82e5ebfa71e
Dean Sofer <proloser@hotmail.com>
2012-02-05 17:53:00 -0800

    Added pagination to homepage

--- Controller/ActivitiesController.php
+++ Controller/ActivitiesController.php
@@ -3,19 +3,21 @@ class ActivitiesController extends AppController {
 	public $name = 'Activities';
 	public $helpers = array('Time', 'Text');
 	public $paginate = array('dashboard');
-
-	function index() {
-		$activities = $this->Activity->find('dashboard');
-		$actions = $this->Activity->actions;
-		$this->set(compact('activities', 'actions'));
-	}
 	
-	function paginated() {
+	function index() {
+		if (!isset($this->paginate['limit']))
+			$this->paginate['limit'] = '40';
 		$activities = $this->paginate();
 		$actions = $this->Activity->actions;
 		$paginate = true;
 		$this->set(compact('activities', 'actions', 'paginate'));
 		$this->render('index');
+	}
+
+	function full() {
+		$activities = $this->Activity->find('dashboard');
+		$actions = $this->Activity->actions;
+		$this->set(compact('activities', 'actions'));
 	}
 }
 ?>
